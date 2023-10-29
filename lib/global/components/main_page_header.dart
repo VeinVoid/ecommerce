53f740dbc400955d628954a3bb1e645dd99a5fd1
VeinVoid <vickokudus@gmail.com>
2023-10-29 19:44:58 +0700

    feat : [VA]  add global widget and component

---
+++ lib/global/components/main_page_header.dart
@@ -0,0 +1,33 @@
+import 'package:ecommerce_mobile/utils/thames.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HeaderMainPage extends StatelessWidget {
+  const HeaderMainPage({
+    required this.pageName
+  });
+
+  final String pageName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                pageName,
+                style: tsHeader(fontWeight: FontWeight.w700),
+              ),
+              SvgPicture.asset(
+                'assets/icons/icon_cart.svg'
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/global/widgets/category_card.dart
@@ -0,0 +1,36 @@
+import 'package:ecommerce_mobile/utils/thames.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+Widget categoryCard({
+  required double width,
+  required double height,
+  required String categoryName,
+  required String imageUrl
+  }){
+  return Container(
+    margin: EdgeInsets.all(25),
+    width: width,
+    height: height,
+    decoration: BoxDecoration(
+      color: generalColorWhite,
+      borderRadius: BorderRadius.circular(5),
+      boxShadow: [
+        primaryBackDrop()
+      ],
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SvgPicture.asset(imageUrl),
+        SizedBox(height: 11),
+        Text(
+          categoryName,
+          style: tsParaghraph16(
+            fontWeight: FontWeight.w600
+          ),
+        )
+      ],
+    ),
+  );
+}

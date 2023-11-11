bd70f602f5427d590b9a7ea8f419a52153678781
VeinVoid <vickokudus@gmail.com>
2023-11-12 00:10:41 +0700

    fix : [VA] fix navigate

--- lib/global/widgets/category_card.dart
+++ lib/global/widgets/category_card.dart
@@ -6,31 +6,35 @@ Widget categoryCard({
   required double width,
   required double height,
   required String categoryName,
-  required Uint8List imageBytes
+  required Uint8List imageBytes,
+  required Function() onTap
   }){
-  return Container(
-    margin: EdgeInsets.symmetric(horizontal: width * 0.08, vertical: height * 0.1),
-    width: width,
-    height: height,
-    decoration: BoxDecoration(
-      color: generalColorWhite,
-      borderRadius: BorderRadius.circular(5),
-      boxShadow: [
-        primaryBackDrop()
-      ],
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.memory(imageBytes),
-        SizedBox(height: 11),
-        Text(
-          categoryName,
-          style: tsParaghraph16(
-            fontWeight: FontWeight.w600
-          ),
-        )
-      ],
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+      margin: EdgeInsets.symmetric(horizontal: width * 0.08, vertical: height * 0.1),
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: generalColorWhite,
+        borderRadius: BorderRadius.circular(5),
+        boxShadow: [
+          primaryBackDrop()
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.memory(imageBytes),
+          SizedBox(height: 11),
+          Text(
+            categoryName,
+            style: tsParaghraph16(
+              fontWeight: FontWeight.w600
+            ),
+          )
+        ],
+      ),
     ),
   );
 }
